e5b2b2e2976fda106b1f18c9ebcec0faa087104f
Nimesha Hansani Amarasinghe <nimesha@3rivetech.com>
2025-08-15 20:36:30 +0530

    Python File changed

--- Flight Data API/FetchFlightDatafromAPI.py
+++ Flight Data API/FetchFlightDatafromAPI.py
@@ -101,4 +101,4 @@ def get_flights():
 # Run locally
 if __name__ == "__main__":
    
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run(app, host="0.0.0.0", port=8080)
