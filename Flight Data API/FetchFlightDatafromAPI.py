3ed7a40cfa605bc5033e020d9e2f27f42a86c680
Nimesha Hansani Amarasinghe <nimesha@3rivetech.com>
2025-08-15 20:29:41 +0530

    Changed

--- Flight Data API/FetchFlightDatafromAPI.py
+++ Flight Data API/FetchFlightDatafromAPI.py
@@ -5,6 +5,7 @@ import threading
 import time
 import os
 from datetime import datetime, timezone
+import uvicorn
 
 app = FastAPI()
 
@@ -99,5 +100,5 @@ def get_flights():
 
 # Run locally
 if __name__ == "__main__":
-    import uvicorn
+   
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
